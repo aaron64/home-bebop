c3b827e1ebe7be142418e1ccd05ffc29bda6385c
Aaron Chambers <aaron.chambers81@gmail.com>
2018-10-14 17:05:29 -0400

    added ascii art and modified run to handle smoketests as well as light works

--- run.sh
+++ run.sh
@@ -1,22 +1,4 @@
 
-RUNNING=true
-ERROR_TEXT=""
-TEST_NAME=""
+cat dexter.txt
 
-sendError() {
-	./sendMessage.sh "Error testing $TEST_NAME: $ERROR_TEXT"
-}
-
-runTest() {
-	TEST_NAME=$1
-	echo "Testing: $TEST_NAME"
-	. ./tests/test${TEST_NAME}.sh || sendError
-}
-
-
-while [ $RUNNING == true ]
-do
-	runTest Ein		
-	runTest PiHole
-	sleep 600
-done
+./run_tests.sh
--- tests/testEin.sh
+++ tests/testEin.sh
@@ -1,2 +1,6 @@
-return 1
-echo "hi"
+./getCredential.sh ip_ein
+ping $ip_ein
+
+if [[ $? =~ 0 ]]; then
+	echo "hi"
+fi
